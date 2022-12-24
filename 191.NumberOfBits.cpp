c3e7ad70a2d09c3bf7030dad3b12fcdb2489e6f2
Dev-AyanB <contactdevayan@gmail.com>
2022-12-24 17:02:14 +0530

    added p191

---
+++ 191.NumberOfBits.cpp
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <iostream>
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            if (n & 1)
+            {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+};
+
